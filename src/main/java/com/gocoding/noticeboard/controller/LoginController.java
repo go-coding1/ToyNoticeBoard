7fc429147fa65a0384417a6d6d8642a222d0d312
go-coding1 <rhtjrwnszyzy@gmail.com>
2021-05-22 11:38:39 +0900

    회원가입 화면 작성

--- src/main/java/com/gocoding/noticeboard/controller/LoginController.java
+++ src/main/java/com/gocoding/noticeboard/controller/LoginController.java
@@ -1,10 +1,16 @@
 package com.gocoding.noticeboard.controller;
 
 import com.gocoding.noticeboard.entity.Member;
+import com.gocoding.noticeboard.entity.ROLE;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -13,6 +19,16 @@ public class LoginController {
     @GetMapping("/login")
     public void getLogin(){
 
+    }
+
+    @GetMapping("/register")
+    public String getRegister(Model model){
+        List<ROLE> role = new ArrayList<>();
+        role.add(ROLE.ROLE_USER);
+        role.add(ROLE.ROLE_MANAGER);
+        role.add(ROLE.ROLE_ADMIN);
+        model.addAttribute("list",role);
+        return "register";
     }
 
 }
